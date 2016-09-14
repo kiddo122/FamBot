22476cacd213d8bf24a30e745baa0a05ec2dae73
Kaibo Ma <kaiboma@dhcp-206-87-196-244.ubcsecure.wireless.ubc.ca>
2016-09-14 11:41:10 -0700

    first commit

---
+++ fambot/index.js
@@ -0,0 +1,52 @@
+var env = require('../config.json'),
+    Help = require('./Help.js'),
+    Imgur = require('./Imgur.js'),
+    Giphy = require('./Giphy.js'),
+    Urban = require('./Urban.js');
+
+var FamBot = function () {
+    this.keywords = env.keywords;
+    this.Help = new Help;
+    this.Imgur = new Imgur;
+    this.Giphy = new Giphy;
+    this.Urban = new Urban;
+};
+
+FamBot.prototype.loadKeywords = function ()
+{
+    var result = [];
+    for (var i in this.keywords) {
+        if (this.keywords.hasOwnProperty(i)) {
+            result.push(this.keywords[i]);
+        }
+    }
+    return result;
+}
+
+FamBot.prototype.checkMessageForKeywords = function(message, triggers, callback)
+{
+    for(var i = 0; i != triggers.length; i++) {
+        var substring = triggers[i];
+        if (message.indexOf(substring) == 0) {
+            return callback(substring);
+        }
+    }
+    return callback(0);
+}
+
+FamBot.prototype.getKeyByValue = function(object, value)
+{
+    for(var prop in object) {
+        if(object.hasOwnProperty(prop)) {
+            if(object[prop] == value)
+                return prop;
+        }
+    }
+}
+
+FamBot.prototype.runKeywordFunction = function(keywordFunction, keyword, message, callback)
+{
+    this[keywordFunction].Message(keyword, message, callback);
+}
+
+module.exports = FamBot;
---
+++ index.js
@@ -0,0 +1,32 @@
+var env = require('./config.json'),
+    FamBot = require('./fambot/index.js'),
+    Discord = require('discord.js');
+
+var ins = new FamBot;
+var discordjs = new Discord.Client();
+
+discordjs.on("ready", function () {
+    console.log("Ready to begin! Serving in " + discordjs.channels.length + " channels");
+});
+
+discordjs.on('message', function(msg)
+{
+    if (typeof ins.loadKeywords() !== 'undefined' && ins.loadKeywords().length > 0) {
+        ins.checkMessageForKeywords(msg.content, ins.loadKeywords(), function(keyword)
+        {
+            if (keyword != 0) {
+                ins.runKeywordFunction(ins.getKeyByValue(ins.keywords, keyword), keyword, msg, function(reply)
+                {
+                    discordjs.reply(msg, reply);
+                });
+            }
+        });
+    }
+});
+
+discordjs.on('disconnected', function () {
+    console.log('Disconnected.');
+    process.exit(1);
+});
+
+discordjs.login(env.discord.email, env.discord.password);
